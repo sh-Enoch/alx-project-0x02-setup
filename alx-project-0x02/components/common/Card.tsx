575a919d68716691364fabf24263ba6404b90454
sh-Enoch <wekesawafula.enoch@gmail.com>
2025-12-09 13:26:30 +0300

    fix: update import statement for CardProps in Card component

--- alx-project-0x02/components/common/Card.tsx
+++ alx-project-0x02/components/common/Card.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { CardProps } from "../../interfaces";
-
+// import { CardProps } from "../../interfaces";
+import { type CardProps } from "../../interfaces";
 function Card({ title, content }: CardProps) {
   return <div>Card</div>;
 }
