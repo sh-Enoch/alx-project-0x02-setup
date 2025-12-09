43cff8d2083617ac0eb54d0e6c3cdbb6653484cf
sh-Enoch <wekesawafula.enoch@gmail.com>
2025-12-09 13:24:24 +0300

    fix: update import path for Card component in home page

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import Card from "../components/common/Card";
+import Card from "@/components/common/Card";
 
 function home() {
-  return (<div>
-    <Card title="Sample Title" content="Sample Content" />
-  </div>);
+  return (
+    <div>
+      <Card title="Sample Title" content="Sample Content" />
+    </div>
+  );
 }
 
 export default home;
