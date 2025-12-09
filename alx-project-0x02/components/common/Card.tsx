e6c0cc91e7fffefad14e9663f02662bbb50adc0c
sh-Enoch <wekesawafula.enoch@gmail.com>
2025-12-09 13:22:43 +0300

    feat: add Card component and interface, update home page to use Card

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CardProps } from "../../interfaces";
+
+function Card({ title, content }: CardProps) {
+  return <div>Card</div>;
+}
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import Card from "../components/common/Card";
 
 function home() {
-  return <div>home</div>;
+  return (<div>
+    <Card title="Sample Title" content="Sample Content" />
+  </div>);
 }
 
 export default home;
